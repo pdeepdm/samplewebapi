deac7e59c0a7a34bf9e13643c1a31b613369dffc
Pradeepkumar Megalamani <pdeepdm@gmail.com>
2024-05-20 19:00:16 +0530

    source files of sample webapi

---
+++ SampleWebApi/ItemsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWebApi
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class ItemsController:ControllerBase
+    {
+        private static List<string> mItemsData = new List<string>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetItems() 
+        {
+            return mItemsData;
+        
+        }
+
+        [HttpPost]
+        public ActionResult AddItem([FromBody] string item)
+        {
+            mItemsData.Add(item);
+            return Ok();
+        }
+    }
+}
---
+++ SampleWebApi/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+Console.WriteLine("Hello, World!");
+
+WebHost.CreateDefaultBuilder()
+    .UseKestrel()
+    .ConfigureServices(services => { services.AddMvc(); })
+    .Configure(config => { config.UseMvc(); })
+    .UseUrls("http://*:5000")
+    .Build()
+    .Run();
+Console.ReadLine();
